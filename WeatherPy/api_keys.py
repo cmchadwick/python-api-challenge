da0aac1821d52c0b874f584f8fbc7186ef83daf6
cmchadwick <connorchadwick@Connors-MacBook-Pro.local>
2024-08-28 15:50:16 -0500

    Finished WeatherPy, pushing to remote

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "b094875b62847d49633c5c796fa44e25"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "c6d83cdd7dd841148f3f0503337dc36b"
